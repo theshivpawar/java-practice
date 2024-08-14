027ed50604ae40d3cc0dd775a7855d1ac286c40c
Shivam Pawar <theshivpawar@gmail.com>
2024-08-14 09:44:42 +0530

    First Java Program

---
+++ src/fundamentals/getting_started/HelloWorld.java
@@ -0,0 +1,11 @@
+package fundamentals.getting_started;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        // This is a comment
+
+    }
+}
