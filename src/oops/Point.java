95c443f20bb7184687e04b8cb077f44064dfffca
Shivam Pawar <theshivpawar@gmail.com>
2024-09-03 20:00:19 +0530

    New

---
+++ src/oops/Point.java
@@ -0,0 +1,19 @@
+package oops;
+
+import java.util.Arrays;
+
+public class Point {
+    // Fields
+    public int x;
+
+    public static void main(String[] args) {
+
+        int[] numbers; // local variables
+
+        numbers = new int[5];
+
+        System.out.println(Arrays.toString(numbers));
+    }
+
+
+}
